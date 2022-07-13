6dd45c14356a756abaa9d0ca860804ef9fedb806
thanai-mac <62784012+thanaism@users.noreply.github.com>
2022-07-14 07:47:34 +0900

    絵文字選択用のパッドを追加

---
+++ src/components/EmojiPicker.tsx
@@ -0,0 +1,30 @@
+import { FC } from 'react';
+import Picker, { IEmojiPickerProps } from 'emoji-picker-react';
+import {
+  Button,
+  Icon,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverCloseButton,
+  PopoverContent,
+  PopoverTrigger,
+} from '@chakra-ui/react';
+import { BsEmojiSunglasses } from 'react-icons/bs';
+
+const EmojiPickerPopover: FC<IEmojiPickerProps> = (props) => (
+  <Popover>
+    <PopoverTrigger>
+      <Button rounded={'full'}>
+        <Icon as={BsEmojiSunglasses} />
+      </Button>
+    </PopoverTrigger>
+    <PopoverContent maxWidth={0} borderStyle="none">
+      <PopoverCloseButton />
+      {/* <PopoverArrow /> */}
+      <Picker {...props} />
+    </PopoverContent>
+  </Popover>
+);
+
+export default EmojiPickerPopover;
