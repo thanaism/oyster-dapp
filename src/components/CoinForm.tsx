9a90bc93172717d7f5765eed0b1561410ecd478a
thanai-mac <62784012+thanaism@users.noreply.github.com>
2022-07-14 07:49:20 +0900

    ツイート投稿用のUIを実装

--- src/components/CoinForm.tsx
+++ src/components/CoinForm.tsx
@@ -2,17 +2,26 @@ import {
   Button,
   FormControl,
   FormLabel,
-  Heading,
+  Icon,
   Input,
   Stack,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
   Text,
   useColorModeValue,
 } from '@chakra-ui/react';
+import { BsTwitter } from 'react-icons/bs';
+import { FaShopify } from 'react-icons/fa';
 import { FC, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { MetaMaskState } from 'store/metamaskSlice';
 import { transfer } from 'utils/functions';
 import { ChainIds } from 'utils/metamask';
+import TwitterLogin from './TwitterLogin';
+import FirebaseAuth from './FirebaseAuth';
 
 export const CoinForm: FC = () => {
   const [enabled, setEnabled] = useState(true);
@@ -20,13 +29,14 @@ export const CoinForm: FC = () => {
   const [order, setOrder] = useState('');
   const metamask = useSelector((state: MetaMaskState) => state);
 
+  // eslint-disable-next-line no-shadow
   const requestTransfer = async (to: string, amount: string, phone: string, order: string) => {
     const response = await transfer({ to, amount, phone, order });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-assignment
     const { error, hash } = response.data as any;
     if (hash != null) alert('申請が受理されました。しばらくすると残高に反映されます。');
     if (error != null) alert(error);
   };
-
   return (
     <>
       <Stack
@@ -34,68 +44,94 @@ export const CoinForm: FC = () => {
         w="full"
         maxW="lg"
         bg={useColorModeValue('white', 'gray.700')}
-        rounded="xl"
-        boxShadow="lg"
-        p={6}
+        rounded={{ base: 'none', md: 'xl' }}
+        boxShadow={{ base: 'none', md: 'lg' }}
+        p={{ base: 0, md: 6 }}
         my={12}
       >
-        <Heading lineHeight={1.1} fontSize={{ base: '2xl', md: '3xl' }}>
-          牡蠣の購入者はこちら
-        </Heading>
-        <FormControl id="order" isRequired>
-          <FormLabel>注文番号（4ケタ）</FormLabel>
-          <Input
-            placeholder="0000"
-            _placeholder={{ color: 'gray.500' }}
-            type="number"
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-            onChange={(event: any) => setOrder(event.currentTarget.value)}
-          />
-        </FormControl>
-        <FormControl id="tel" isRequired>
-          <FormLabel>注文時に入力した電話番号</FormLabel>
-          <Input
-            placeholder="09012345678"
-            type="tel"
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-            onChange={(event: any) => setPhone(event.currentTarget.value)}
-          />
-        </FormControl>
-        <Stack spacing={6}>
-          <Button
-            rounded={'full'}
-            bg="blue.400"
-            color="white"
-            _hover={{
-              bg: 'blue.500',
-            }}
-            isLoading={!enabled}
-            onClick={async () => {
-              setEnabled(false);
-              try {
-                await requestTransfer(metamask?.account as string, '5', phone, order);
-              } catch {
-                alert('取得に失敗しました');
-              } finally {
-                setEnabled(true);
-              }
-            }}
-          >
-            牡蠣コイン（テスト版）を申請
-          </Button>
-          <Text fontSize={{ base: 'sm', lg: 'sm' }} color={'gray.400'}>
-            ※2022/5/8以前にご注文の方はシステム未対応のため、
-            <Text as="a" href="https://twitter.com/naokichipocket" textDecorationLine={'underline'}>
-              なおきち
-            </Text>
-            までウォレットアドレスを直接ご連絡ください。
-          </Text>
-        </Stack>
+        <Tabs>
+          <TabList>
+            <Tab>
+              <Icon as={FaShopify} mr={'1'} />
+              牡蠣購入でコイン獲得
+            </Tab>
+            <Tab>
+              <Icon as={BsTwitter} mr={'1'} />
+              ツイートでコイン獲得
+            </Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              {/* <Heading lineHeight={1.1} fontSize={{ base: '2xl', md: '3xl' }}>
+                牡蠣の購入者はこちら
+              </Heading> */}
+              <Stack spacing={6}>
+                <Text>牡蠣若手の会ECサイトで牡蠣や加工品を購入いただいた方が申請できます！</Text>
+                <FormControl id="order" isRequired>
+                  <FormLabel>注文番号（4ケタ）</FormLabel>
+                  <Input
+                    placeholder="0000"
+                    _placeholder={{ color: 'gray.500' }}
+                    type="number"
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
+                    onChange={(event: any) => setOrder(event.currentTarget.value)}
+                  />
+                </FormControl>
+                <FormControl id="tel" isRequired>
+                  <FormLabel>注文時に入力した電話番号</FormLabel>
+                  <Input
+                    placeholder="09012345678"
+                    type="tel"
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
+                    onChange={(event: any) => setPhone(event.currentTarget.value)}
+                  />
+                </FormControl>
+                <Button
+                  rounded={'full'}
+                  bg="blue.400"
+                  color="white"
+                  _hover={{
+                    bg: 'blue.500',
+                  }}
+                  isLoading={!enabled}
+                  onClick={async () => {
+                    setEnabled(false);
+                    try {
+                      await requestTransfer(metamask?.account as string, '5', phone, order);
+                    } catch {
+                      alert('取得に失敗しました');
+                    } finally {
+                      setEnabled(true);
+                    }
+                  }}
+                >
+                  KAKIコイン（Beta版）を申請
+                </Button>
+                <Text fontSize={{ base: 'sm', lg: 'sm' }} color={'gray.400'}>
+                  ※2022/5/8以前にご注文の方はシステム未対応のため、
+                  <Text
+                    as="a"
+                    href="https://twitter.com/naokichipocket"
+                    textDecorationLine={'underline'}
+                  >
+                    なおきち
+                  </Text>
+                  までウォレットアドレスを直接ご連絡ください。
+                </Text>
+              </Stack>
+            </TabPanel>
+            <TabPanel>
+              <FirebaseAuth>
+                <TwitterLogin />
+              </FirebaseAuth>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
       </Stack>
       {metamask?.chainId !== ChainIds.MumbaiTestNet ? (
         <>
           <Text fontSize={{ base: 'sm', lg: 'sm' }} color={'gray.400'}>
-            <Text>現在、牡蠣コインはテスト版としてMumbaiネットワークで運用されています。</Text>
+            <Text>現在、KAKIコインはテスト版としてMumbaiネットワークで運用されています。</Text>
             <Text>MumbaiはPolygon準拠のテスト用ネットワークで利用に一切の費用はかかりません。</Text>
           </Text>
           <Text fontSize={{ base: 'sm', lg: 'sm' }} color={'gray.400'}>
@@ -104,7 +140,7 @@ export const CoinForm: FC = () => {
         </>
       ) : (
         <Text fontSize={{ base: 'sm', lg: 'sm' }} color={'gray.400'}>
-          💡MetaMask上でコインの枚数を確認するには「牡蠣コインを設定」します。
+          💡MetaMask上でコインの枚数を確認するには「KAKIコインを設定」します。
         </Text>
       )}
     </>
--- src/components/TwitterLogin.tsx
+++ src/components/TwitterLogin.tsx
@@ -1,59 +1,171 @@
-import { Heading } from '@chakra-ui/react';
+import { Button, FormControl, FormLabel, Icon, Stack, Textarea } from '@chakra-ui/react';
+import { auth } from 'utils/firebase';
 import { AuthContext, UserContext } from 'contexts/auth';
 import {
-  AdditionalUserInfo,
-  getAdditionalUserInfo,
+  getAuth,
+  linkWithPopup,
+  linkWithRedirect,
   TwitterAuthProvider,
+  unlink,
   UserCredential,
 } from 'firebase/auth';
-import { useContext, useState, VFC } from 'react';
+import { useContext, useState, FC, useEffect } from 'react';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import { FaUndo } from 'react-icons/fa';
+import { tweet } from 'utils/functions';
+import { useSelector } from 'react-redux';
+import { MetaMaskState } from 'store/metamaskSlice';
+import EmojiPickerPopover from './EmojiPicker';
 
-const TwitterLogin: VFC = () => {
+const TwitterLogin: FC = () => {
+  // eslint-disable-next-line no-shadow
   const { auth } = useContext(AuthContext);
   const { credential, setCredential } = useContext(UserContext);
-  const [message, setMessage] = useState('');
-  // const history = useHistory();
-  const [userInfo, setUserInfo] = useState<AdditionalUserInfo | null>(null);
+  const [enabled, setEnabled] = useState(true);
 
   const uiConfig: firebaseui.auth.Config = {
-    // signInFlow: 'redirect',
     signInFlow: 'redirect',
+    // signInFlow: 'popup',
     signInOptions: [
       {
         provider: TwitterAuthProvider.PROVIDER_ID,
         customParameters: { lang: 'ja' },
       },
     ],
-    // signInSuccessUrl: '',
     callbacks: {
       signInSuccessWithAuthResult: (authResult) => {
-        setMessage('Twitterでログインしました');
         setCredential(authResult as UserCredential);
-        const additonalUserInfo = authResult.additionalUserInfo as AdditionalUserInfo;
-        setUserInfo(additonalUserInfo);
-        console.log(authResult);
-        console.log(additonalUserInfo);
         return false;
       },
     },
-    // callbacks: {
-    //   signInSuccessWithAuthResult: (authResult, redirectUrl) => {
-    //     setCredential(authResult as UserCredential);
-    //     console.log(redirectUrl);
-    //     // const dest = redirectUrl || paths.home;
-    //     // history.replace(dest);
-    //     return false;
-    //   },
-    // },
   };
 
+  const baseMessage =
+    '【牡蠣業界を稼げる憧れの業界にする🦪】\n\n' +
+    '牡蠣業界を応援するとKAKIコインがもらえるプロジェクトに参加しました！\n' +
+    'https://oyster-773ce.web.app/';
+  const hashTags = ['#AbyssCrypto', '#牡蠣若手の会', '#KAKIコイン', '#EatOystersToEarn'];
+  // eslint-disable-next-line prefer-template
+  const hashTag = () => '\n\n' + hashTags[Math.floor(Math.random() * hashTags.length)];
+  const [text, setText] = useState(baseMessage + hashTag());
+  const [start, setStart] = useState(0);
+
+  const metamask = useSelector((state: MetaMaskState) => state);
+
+  const requestTweet = async (data: {
+    to: string;
+    accessToken: string;
+    accessSecret: string;
+    text: string;
+  }) => {
+    const { to, accessToken, accessSecret, text } = data;
+    const response = await tweet({ to, accessSecret, accessToken, text });
+    const { error, hash } = response.data as any;
+    if (hash != null) alert('申請が受理されました。しばらくすると残高に反映されます。');
+    if (error != null) alert(error);
+  };
+
+  useEffect(() => {
+    const user = getAuth().currentUser;
+    if (user != null) {
+      const { providerData } = user;
+      console.log('providerData', providerData);
+    }
+  });
   return (
     <>
       <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
-      <Heading>{message}</Heading>
-      {/* {JSON.stringify(credential)}
-      <Heading>{JSON.stringify(userInfo)}</Heading> */}
+      {/* <Button
+        onClick={async () => {
+          const provider = new TwitterAuthProvider();
+          const user = getAuth().currentUser;
+          if (user != null) {
+            // const result = await linkWithPopup(user, provider);
+            const result = await linkWithRedirect(user, provider);
+            console.log(result);
+          }
+        }}
+      >
+        link
+      </Button>
+      <Button
+        onClick={async () => {
+          const user = getAuth().currentUser;
+          if (user != null) {
+            await unlink(user, 'twitter.com');
+          }
+        }}
+      >
+        unlink
+      </Button> */}
+      {credential ? (
+        <Stack spacing={6}>
+          {/* <Text>アカウント：{JSON.stringify((credential as any)?.credential.accessToken)}</Text> */}
+          <FormControl>
+            <FormLabel>投稿内容</FormLabel>
+            <Textarea
+              aria-multiline
+              minH={'3xs'}
+              value={text}
+              onInput={(event: any) => {
+                setText(event.target.value as string);
+                setStart(event.target.selectionStart as number);
+              }}
+              onClick={(event: any) => setStart(event.target.selectionStart as number)}
+            />
+          </FormControl>
+          <Stack direction={'row-reverse'}>
+            <Button
+              rounded={'full'}
+              bg="blue.400"
+              color="white"
+              isLoading={!enabled}
+              _hover={{ bg: 'blue.500' }}
+              onClick={async () => {
+                setEnabled(false);
+                try {
+                  const to = metamask.account!;
+                  console.log(JSON.stringify((credential as any).credential));
+                  const { accessToken, secret: accessSecret } = (credential as any).credential;
+                  await requestTweet({ to, accessToken, accessSecret, text });
+                } catch {
+                  alert('取得に失敗しました');
+                } finally {
+                  setEnabled(true);
+                }
+                // alert(`ツイート処理を実装予定です。\n\n投稿内容：\n${text}`);
+                setText(baseMessage + hashTag());
+                setStart(0);
+              }}
+            >
+              {/* ツイートしてKAKIコインを獲得！ */}
+              ツイートして獲得
+            </Button>
+            <EmojiPickerPopover
+              onEmojiClick={(event, obj) => {
+                const leftHand = text.slice(0, start);
+                const rightHand = text.slice(start);
+                setStart(obj.emoji.length + start);
+                setText(leftHand + obj.emoji + rightHand);
+              }}
+            />
+            <Button
+              bg="red.400"
+              color="white"
+              _hover={{ bg: 'red.500' }}
+              rounded={'full'}
+              onClick={() => {
+                setText(baseMessage + hashTag());
+                setStart(0);
+              }}
+            >
+              <Icon as={FaUndo} />
+            </Button>
+          </Stack>
+        </Stack>
+      ) : (
+        ''
+      )}
     </>
   );
 };
