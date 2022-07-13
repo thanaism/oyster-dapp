bb8b8d28def72e2f70a5430723bb7203df9bf383
thanai-mac <62784012+thanaism@users.noreply.github.com>
2022-07-14 07:46:23 +0900

    ページ更新時の再ログインを省略できるようにuseEffect処理を更新

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,31 @@
 /* eslint-disable react/jsx-no-constructed-context-values */
-import { useEffect, FC, useState } from 'react';
+import { FC, useEffect } from 'react';
 import { initializeEthereum } from 'utils/metamask';
 import { useDispatch, useSelector } from 'react-redux';
-import { MetaMaskState } from 'store/metamaskSlice';
-import { ChakraProvider, Text } from '@chakra-ui/react';
+import { metamaskSlice, MetaMaskState } from 'store/metamaskSlice';
+import { ChakraProvider } from '@chakra-ui/react';
 import { getAuth } from 'firebase/auth';
 import { SplitScreen } from 'components/SplitScreen';
-import TwitterLogin from 'components/TwitterLogin';
-import FirebaseAuth from 'components/FirebaseAuth';
-import EmojiPickerPopover from 'components/EmojiPicker';
 import Fonts from 'utils/fonts';
 
 const App: FC = () => {
   const dispatch = useDispatch();
   const metamask = useSelector((state: MetaMaskState) => state);
+  const { actions } = metamaskSlice;
 
   useEffect(() => {
     void initializeEthereum(dispatch, metamask);
+    console.log('currentUser', getAuth().currentUser);
     void getAuth().onAuthStateChanged((user) => {
       if (user) {
-        // printUserInfo());
-        // disableLogin
+        dispatch(actions.setUser(user.uid));
       } else {
         // clearUserInfo();
         // disableLogout
       }
     });
-  }, [dispatch, metamask]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <ChakraProvider>
