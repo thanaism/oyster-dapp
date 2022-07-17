a25596b774eb2eac2da2180a5e74130cf90d897c
thanai-mac <62784012+thanaism@users.noreply.github.com>
2022-07-18 07:46:04 +0900

    不要なファイルを削除

--- src/firestoreSample.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from 'react';
-
-const FirestoreSample: FC = () => {
-  const hoge = 'aaa';
-  return <h1>{hoge}</h1>;
-};
-
-export default FirestoreSample;
--- src/utils/convertAbi.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires, import/newline-after-import */
-const utils = require('ethers/lib/utils');
-// const abi = require('../abis/OpenSeaERC1155.json');
-const wabi = require('../abis/KakiCoin.json');
-const iface = new utils.Interface(wabi.abi);
-const readableInterface = iface.format(utils.FormatTypes.full);
-console.log(readableInterface);
