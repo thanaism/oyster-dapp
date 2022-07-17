ba22d7e08a0900a5b50c27743415f3e414470537
thanai-mac <62784012+thanaism@users.noreply.github.com>
2022-07-18 07:47:56 +0900

    不要な行を削除

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,8 +1,6 @@
 import { httpsCallable } from 'firebase/functions';
 import { functions } from './firebase';
 
-// eslint-disable-next-line import/prefer-default-export
-export const test = httpsCallable(functions, 'test');
 export const generateNonce = httpsCallable(functions, 'generateNonce');
 export const verifyNonce = httpsCallable(functions, 'verifyNonce');
 export const deleteNonce = httpsCallable(functions, 'deleteNonce');
