7e0c2ffae1bd22f8ade25df5168d615657ad0a5c
thanai-mac <62784012+thanaism@users.noreply.github.com>
2022-07-18 17:45:45 +0900

    不要な行を削除

--- src/atoms/twitterState.ts
+++ src/atoms/twitterState.ts
@@ -1,7 +1,6 @@
 import { OAuthCredential } from 'firebase/auth';
 import { atom } from 'recoil';
 
-// eslint-disable-next-line import/prefer-default-export
 export const twitterCredential = atom<OAuthCredential | undefined>({
   key: 'TwitterCredential',
   default: undefined,
