a10808fcd1209683f2d3ed24fd8c0e87f89446fb
thanai-mac <62784012+thanaism@users.noreply.github.com>
2022-07-22 06:04:58 +0900

    全体デザインに合わせた微修正

--- src/components/EmojiPicker.tsx
+++ src/components/EmojiPicker.tsx
@@ -3,19 +3,24 @@ import Picker, { IEmojiPickerProps } from 'emoji-picker-react';
 import {
   Button,
   Icon,
+  IconButton,
   Popover,
   PopoverCloseButton,
   PopoverContent,
   PopoverTrigger,
 } from '@chakra-ui/react';
-import { BsEmojiSunglasses } from 'react-icons/bs';
+import { BsEmojiSmile } from 'react-icons/bs';
 
 const EmojiPickerPopover: FC<IEmojiPickerProps> = (props) => (
   <Popover>
     <PopoverTrigger>
-      <Button rounded="full">
-        <Icon as={BsEmojiSunglasses} />
-      </Button>
+      <IconButton
+        aria-label="Input Emoji"
+        isRound
+        variant="outline"
+        _hover={{ opacity: '0.6', color: 'white' }}
+        icon={<Icon as={BsEmojiSmile} />}
+      />
     </PopoverTrigger>
     <PopoverContent maxWidth={0} borderStyle="none">
       <PopoverCloseButton />
