23e3037a89ee99191d17c723db18a5a1b4ed5185
thanai-mac <62784012+thanaism@users.noreply.github.com>
2022-07-22 06:00:02 +0900

    不要な記述の削除

--- src/utils/metamask.ts
+++ src/utils/metamask.ts
@@ -1,28 +1,6 @@
-import { Dispatch } from 'react';
-import { AnyAction } from 'redux';
 import { MetaMaskInpageProvider } from '@metamask/providers';
 
-export const requestAccount = async (): Promise<string | undefined> => {
-  const { ethereum } = window as unknown as { ethereum: MetaMaskInpageProvider };
-
-  if (ethereum == null) return undefined;
-  console.log(ethereum.request.bind(ethereum));
-  const accounts = (await ethereum.request({ method: 'eth_requestAccounts' })) as string[];
-  return accounts.length > 0 ? accounts[0] : undefined;
-};
-
-export const getAccount = async (): Promise<string | undefined> => {
-  const { ethereum } = window as unknown as { ethereum: MetaMaskInpageProvider };
-  if (ethereum == null) return undefined;
-  const accounts = (await ethereum.request({ method: 'eth_accounts' })) as string[];
-  return accounts.length > 0 ? accounts[0] : undefined;
-};
-
-export const hasMetaMask = (): boolean => {
-  const { ethereum } = window as unknown as { ethereum: MetaMaskInpageProvider };
-  if (ethereum == null) return false;
-  return ethereum.isMetaMask;
-};
+const KAKICOIN_CONTRACT_ADDRESS = '0x23f84887e93CDbaCF6126912C34fA452C02e8302';
 
 export const ChainIds = {
   Mainnet: '0x1',
@@ -36,57 +14,59 @@ export const ChainIds = {
 
 type ChainId = typeof ChainIds[keyof typeof ChainIds];
 
+export const requestAccount = async (): Promise<string | undefined> => {
+  const { ethereum } = window as unknown as { ethereum: MetaMaskInpageProvider };
+  if (ethereum == null) return undefined;
+  const accounts = (await ethereum.request({ method: 'eth_requestAccounts' })) as string[];
+  return accounts.length > 0 ? accounts[0] : undefined;
+};
+
+export const getAccount = async (): Promise<string | undefined> => {
+  const { ethereum } = window as unknown as { ethereum: MetaMaskInpageProvider };
+  if (ethereum == null) return undefined;
+  const accounts = (await ethereum.request({ method: 'eth_accounts' })) as string[];
+  return accounts.length > 0 ? accounts[0] : undefined;
+};
+
 export const switchNetwork = async (chainId: ChainId): Promise<void> => {
-  try {
-    const { ethereum } = window as unknown as { ethereum: MetaMaskInpageProvider };
-    await ethereum.request({
-      method: 'wallet_switchEthereumChain',
-      params: [{ chainId }],
-    });
-  } catch (e) {
-    console.log(e);
-  }
+  const { ethereum } = window as unknown as { ethereum: MetaMaskInpageProvider };
+  await ethereum.request({
+    method: 'wallet_switchEthereumChain',
+    params: [{ chainId }],
+  });
 };
 
 export const addMumbaiNetworkToWallet = async (): Promise<void> => {
-  try {
-    const { ethereum } = window as unknown as { ethereum: MetaMaskInpageProvider };
-    await ethereum.request({
-      method: 'wallet_addEthereumChain',
-      params: [
-        {
-          chainId: '0x13881',
-          blockExplorerUrls: ['https://mumbai.polygonscan.com/'],
-          nativeCurrency: {
-            decimals: 18,
-            symbol: 'MATIC',
-          },
-          chainName: 'Mumbai Test Network',
-          rpcUrls: ['https://matic-testnet-archive-rpc.bwarelabs.com'],
+  const { ethereum } = window as unknown as { ethereum: MetaMaskInpageProvider };
+  await ethereum.request({
+    method: 'wallet_addEthereumChain',
+    params: [
+      {
+        chainId: '0x13881',
+        blockExplorerUrls: ['https://mumbai.polygonscan.com/'],
+        nativeCurrency: {
+          decimals: 18,
+          symbol: 'MATIC',
         },
-      ],
-    });
-  } catch (e) {
-    console.log(e);
-  }
+        chainName: 'Mumbai Test Network',
+        rpcUrls: ['https://matic-testnet-archive-rpc.bwarelabs.com'],
+      },
+    ],
+  });
 };
 
-export const addKakiCoinAsset = async (chainId: string): Promise<void> => {
-  try {
-    if (chainId !== ChainIds.MumbaiTestNet) throw Error('Need to switch network...');
-    const { ethereum } = window as unknown as { ethereum: MetaMaskInpageProvider };
-    await ethereum.request({
-      method: 'wallet_watchAsset',
-      params: {
-        type: 'ERC20',
-        options: {
-          address: '0x23f84887e93CDbaCF6126912C34fA452C02e8302',
-          symbol: 'OYSTER',
-          decimals: 18,
-        },
+export const registerKakiCoinWithWallet = async (chainId: string): Promise<void> => {
+  if (chainId !== ChainIds.MumbaiTestNet) throw Error('Need to switch network...');
+  const { ethereum } = window as unknown as { ethereum: MetaMaskInpageProvider };
+  await ethereum.request({
+    method: 'wallet_watchAsset',
+    params: {
+      type: 'ERC20',
+      options: {
+        address: KAKICOIN_CONTRACT_ADDRESS,
+        symbol: 'OYSTER',
+        decimals: 18,
       },
-    });
-  } catch (e) {
-    console.log(e);
-  }
+    },
+  });
 };
