33354fa71d38c2412a348bda8264ea0b8aba02fb
thanai-mac <62784012+thanaism@users.noreply.github.com>
2022-07-22 06:03:18 +0900

    グローバルのCSS適用

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { FC } from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { SplitScreen } from 'components/SplitScreen';
 import { RecoilRoot } from 'recoil';
 import Fonts from 'utils/fonts';
 
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        backgroundColor: '#0D204A',
+        color: 'gray.100',
+      },
+    },
+  },
+});
+
 const App: FC = () => (
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <RecoilRoot>
       <Fonts />
       <SplitScreen />
